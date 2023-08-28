dec8a41cb2528bf1686d26fbd88a8ffea61b1827
pacifiqueCYUBAHIRO <paccymaker@gmail.com>
2023-08-28 18:29:24 +0200

    updates

--- admin.php
+++ admin.php
@@ -16,7 +16,8 @@ $all_data = $conn->query($sql);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8" />
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Attendance Dashboard</title>
   <style>
     /*  import google fonts */
